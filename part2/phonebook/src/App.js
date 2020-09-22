7c19013c3c5da70a709ff56d45fa14623904348a
Khun Zohn <khunzohn@gmail.com>
2020-09-22 07:59:59 +0630

    finish exe 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { 
+      id: 1,
+      name: 'Arto Hellas' 
+    }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      id: persons.length + 1,
+      name: newName
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      
+      {persons.map(value => <p key={value.id}>{value.name}</p>)}
+    </div>
+  )
+}
+
+export default App
