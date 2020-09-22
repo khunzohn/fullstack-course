d8fdbb7cc8c53827079cb6d6455b790c66ddb66e
Khun Zohn <khunzohn@gmail.com>
2020-09-22 22:20:03 +0630

    finish 2.11 fetching phonebook persons from db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { id: 1, name: 'Arto Hellas', number: "09448081677" },
-    { id: 2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id: 3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id: 4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newKeyword, setNewKeyword ] = useState('')
+
+  useEffect(()=> {
+    console.log('effect');
+    axios
+      .get("http://localhost:3002/persons")
+      .then(response => {
+        console.log(response);
+        setPersons(response.data)
+      })
+  },[])
 
   const personsToShow = persons.filter(value => value.name.toLocaleLowerCase().includes(newKeyword.toLocaleLowerCase()))
 
