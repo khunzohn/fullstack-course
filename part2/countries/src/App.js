a34e1670002ed252b22c077e6bc9c1d5ceaec8bc
Khun Zohn <khunzohn@gmail.com>
2020-09-23 00:08:35 +0630

    Move all components to components dir

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Content from './Content';
+import Content from './components/Content';
 import axios from 'axios'
 
 function App() {
