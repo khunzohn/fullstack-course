879ea33bb00445215d34cdccbddb494cb299f0d8
Khun Zohn <khunzohn@gmail.com>
2020-12-06 14:01:48 +0630

    use prod phonebook endpoint

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://arcane-cove-57668.herokuapp.com/api/persons'
 
 const getAll = () => {
     return axios
